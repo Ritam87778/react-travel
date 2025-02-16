c30e9265fafa167fcf7cffd4c5885a8dd864f1bb
Ritam <ritamdutta87778@gmail.com>
2025-02-16 20:08:04 +0530

    services

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "../src/components/Navbar.jsx";
+import ServiceSection from "./components/Services.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ServiceSection/>
     </>
   );
 }
